8d324ca09ec75a3238f9aadd01b18877f4a04572
Steve Strong <steve@srstrong.com>
2010-06-14 23:01:48 +0200

    Initial commit

---
+++ Continuations/Continuations.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Continuations
+{
+    class Continuations
+    {
+        static void Main()
+        {
+            MultiTaskContinuation();
+
+            ContinueWithExceptions();
+        }
+
+        private static void MultiTaskContinuation()
+        {
+            var tasks = new Task<Stream>[2];
+
+            tasks[0] = new Task<Stream>(() => WebRequest.Create("http://www.google.com")
+                                                     .GetResponse()
+                                                     .GetResponseStream());
+
+            tasks[1] = new Task<Stream>(() => WebRequest.Create("http://www.bing.com")
+                                                     .GetResponse()
+                                                     .GetResponseStream());
+
+            tasks[1].Start();
+            tasks[0].Start();
+
+            var task3 = Task.Factory.ContinueWhenAny(tasks, winner =>
+                            {
+                                Console.WriteLine("Final task running");
+                                return winner == tasks[0] ? "Google" : "Bing";
+                            });
+
+            Console.WriteLine("Waiting for tasks to complete");
+
+            // Blocks until task3 completes
+            Console.WriteLine("{0} wins!", task3.Result);
+        }
+
+        private static void ContinueWithExceptions()
+        {
+            var task = new Task<Stream>(() =>
+                                        WebRequest.Create("http://www.goadfogle.com")
+                                                  .GetResponse()
+                                                  .GetResponseStream()
+                );
+
+            var successTask = task.ContinueWith(t => Console.WriteLine("Request successful"),
+                                             TaskContinuationOptions.OnlyOnRanToCompletion);
+
+            var errorTask = task.ContinueWith(t => Console.WriteLine("Task failed: {0}", t.Exception), 
+                                           TaskContinuationOptions.OnlyOnFaulted);
+
+            task.Start();
+
+            try
+            {
+                Task.WaitAll(successTask, errorTask);
+            }
+            catch
+            {
+            }
+
+            task.DisplayState("request task");
+            successTask.DisplayState("success");
+            errorTask.DisplayState("error");
+        }
+    }
+
+    public static class Extensions
+    {
+        public static void DisplayState(this Task task, string name)
+        {
+            Console.WriteLine("{3} state: {{ Completed: {0}, Cancelled: {1}, Faulted: {2} }}", task.IsCompleted, task.IsCanceled, task.IsFaulted, name);
+        }
+    }
+
+}
---
+++ ImageConversionWithProgressIndication/ImageBuffer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace ImageConversionWithProgressIndication
+{
+    class ImageBuffer
+    {
+        private readonly byte[] _buffer;
+        private readonly int _height;
+        private readonly int _width;
+        private readonly int _stride;
+
+        public event Action<byte[], Int32Rect, int, int> RowReady;
+
+        public ImageBuffer(BitmapSource bitmap)
+        {
+            _height = bitmap.PixelHeight;
+            _width = bitmap.PixelWidth;
+            _stride = _width*4;
+            _buffer = new byte[_height * _stride];
+            bitmap.CopyPixels(Int32Rect.Empty, _buffer, _stride, 0);
+        }
+
+        public void ConvertImageToSepia()
+        {
+            for (var row = 0; row < _height; row++)
+            //Parallel.For(0, _height, row =>
+            {
+                for (var column = 0; column < _width; column++)
+                {
+                    var pixel = _buffer.GetPixel(column, row, _stride);
+
+                    pixel = pixel.ToSepia();
+
+                    _buffer.SetPixel(column, row, _stride, pixel);
+                }
+
+                RowReady(_buffer, new Int32Rect(0, row, _width, 1), _stride,
+                        _buffer.GetOffset(0, row, _stride));
+            }
+            //);
+        }
+    }
+}
